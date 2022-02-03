71c0397ea40e37aca2fef6d905f8e9eb01fcd067
toughbuffalo <syedshoaibali97@gmail.com>
2022-02-02 21:42:14 -0600

    increased fontsize of name and skills

--- components/homepage/IntroBanner.js
+++ components/homepage/IntroBanner.js
@@ -20,7 +20,7 @@ export default function IntroBanner() {
   return (
     <section className='bg-primary-purple-dark p-8 flex flex-col items-center space-y-3 min-h-screen justify-center'>
       <article>
-        <div className='relative w-24 h-24 border-4 border-white rounded-full'>
+        <div className='relative w-32 h-32 border-4 border-white rounded-full'>
           <Image
             className='rounded-full'
             src={`https:${profileImage}`}
@@ -30,12 +30,12 @@ export default function IntroBanner() {
         </div>
       </article>
       <article className='flex flex-col text-primary-purple-light items-center space-y-1'>
-        <h3 className='capitalize text-lg  font-titillium-web'>{fullName}</h3>
+        <h3 className='capitalize text-2xl  font-titillium-web'>{fullName}</h3>
         <div className='flex xs:flex-col  divide-x divide-primary-purple-light divide-opacity-70'>
           {skills.map((skill, index, arr) => (
             <p
               key={skill + index}
-              className='font-poppins text-primary-purple-light pr-2 pl-2 text-opacity-70  font-normal capitalize text-xs tracking-wider'
+              className='font-poppins text-primary-purple-light pr-2 pl-2 text-opacity-70  font-normal capitalize text-base tracking-wider'
             >
               {skill}
             </p>
