4b6979b4641cf6d1ea641b0d75597eb3babffdc1
toughbuffalo <syedshoaibali97@gmail.com>
2022-02-04 17:44:06 -0600

    added view port meta tag to prevent zoom on mobile

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,10 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&family=Titillium+Web:wght@300;400;600&display=swap'
             rel='stylesheet'
           />
+          <meta
+            name='viewport'
+            content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
+          />
         </Head>
         <body className='font-poppins'>
           <Main />
