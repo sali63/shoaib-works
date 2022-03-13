957f8175b2271306c089b79e46546d2d8ea6c1d9
toughbuffalo <syedshoaibali97@gmail.com>
2022-03-13 16:13:06 -0500

    added social icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,6 @@
 import { useGlobalContext } from '../contexts/AppContext';
+import { LinkedIn, Github } from './SVG';
+import linksUrl from '../links/links.json';
 
 export default function Footer() {
   const {
@@ -21,13 +23,23 @@ export default function Footer() {
       {/* add in footer or close to a resource of freepik */}
 
       <a
-        className='hidden md:block text-primary-purple text-opacity-50 text-center row-start-5 col-span-4'
+        className='hidden md:block text-primary-purple text-opacity-50 text-center row-start-5 col-start-3 col-span-4'
         href={pexelUrl}
         target='_blank'
       >
         <span className='tracking-tighter '>{anchorText}</span>
       </a>
-      <div className='text-primary-purple text-opacity-80 self-end row-start-6 col-span-4 text-center'>
+
+      <div className='space-x-2 flex items-end row-start-5 col-start-2 md:row-start-6 md:col-start-6 col-span-2 justify-center'>
+        <a href={linksUrl.linkedin} target='_blank'>
+          <LinkedIn fill='#ffffff' />
+        </a>
+        <a href={linksUrl.github} target='_blank'>
+          <Github fill='#ffffff' />
+        </a>
+      </div>
+
+      <div className='text-primary-purple text-opacity-80 self-end row-start-6 md:col-start-3 col-span-4 text-center'>
         &copy; Copyright {new Date().getFullYear()},<span>&nbsp;</span>
         <span className='capitalize'>{siteName}</span>
       </div>
